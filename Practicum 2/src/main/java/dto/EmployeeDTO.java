b577c8fd004072339d7e960818dbca705962e435
WeiXian <weixian.chew98@gmail.com>
2025-12-03 14:07:26 +0800

    Edit DTO javadoc comments

--- Practicum 2/src/main/java/dto/EmployeeDTO.java
+++ Practicum 2/src/main/java/dto/EmployeeDTO.java
@@ -3,18 +3,47 @@ package dto;
 import java.time.LocalDate;
 
 /**
- * EmployeeDTO is a data transfer object for the Employee entity.
- * It is used to transfer data between the client and the server.
- * It is not an entity itself, but it is used to transfer data between the client and the server.
- * It is not persisted to the database.
- * @author Ilyas & Wei Xian
+ * **EmployeeDTO** is a **Data Transfer Object (DTO)** for conveying essential employee information.
+ *
+ * <p>It is used primarily to transfer a subset of data from the {@link entities.Employee Employee entity}
+ * between the server and client, often to reduce payload size or hide sensitive fields
+ * (like salary, birth date, or gender) that are not needed by the client.</p>
+ *
+ * * This class is a simple object containing only data fields, constructors, and accessor methods.
+ * * It is **not persisted** to the database.
+ * * @author Ilyas & Wei Xian
+ *
  */
 public class EmployeeDTO {
+
+    /**
+     * The unique employee number.
+     */
     private int emp_no;
+
+    /**
+     * The employee's first name.
+     */
     private String first_name;
+
+    /**
+     * The employee's last name.
+     */
     private String last_name;
+
+    /**
+     * The employee's hire date.
+     */
     private LocalDate hire_date;
 
+    /**
+     * Parameterized constructor for creating an {@code EmployeeDTO} instance.
+     *
+     * @param emp_no The employee number.
+     * @param first_name The employee's first name.
+     * @param last_name The employee's last name.
+     * @param hire_date The employee's hire date.
+     */
     public EmployeeDTO(int emp_no, String first_name, String last_name, LocalDate hire_date) {
         this.emp_no = emp_no;
         this.first_name = first_name;
@@ -22,34 +51,66 @@ public class EmployeeDTO {
         this.hire_date = hire_date;
     }
 
+    /**
+     * Retrieves the employee number.
+     * @return The employee number.
+     */
     public int getEmp_no() {
         return emp_no;
     }
 
+    /**
+     * Retrieves the employee's first name.
+     * @return The first name.
+     */
     public String getFirst_name() {
         return first_name;
     }
-    
+
+    /**
+     * Retrieves the employee's last name.
+     * @return The last name.
+     */
     public String getLast_name() {
         return last_name;
     }
 
+    /**
+     * Retrieves the employee's hire date.
+     * @return The hire date.
+     */
     public LocalDate getHire_date() {
         return hire_date;
     }
 
+    /**
+     * Sets the employee number.
+     * @param emp_no The employee number to set.
+     */
     public void setEmp_no(int emp_no) {
         this.emp_no = emp_no;
     }
 
+    /**
+     * Sets the employee's first name.
+     * @param first_name The first name to set.
+     */
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
-    
+
+    /**
+     * Sets the employee's last name.
+     * @param last_name The last name to set.
+     */
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
 
+    /**
+     * Sets the employee's hire date.
+     * @param hire_date The hire date to set.
+     */
     public void setHire_date(LocalDate hire_date) {
         this.hire_date = hire_date;
     }
--- Practicum 2/src/main/java/dto/PromotionDTO.java
+++ Practicum 2/src/main/java/dto/PromotionDTO.java
@@ -1,20 +1,52 @@
 package dto;
 
 /**
- * PromotionDTO is a data transfer object for the Promotion entity.
- * It is used to transfer data between the client and the server.
- * It is not an entity itself, but it is used to transfer data between the client and the server.
- * It is not persisted to the database.
- * @author Ilyas & Wei Xian
+ * **PromotionDTO** is a **Data Transfer Object (DTO)** specifically designed to encapsulate
+ * the necessary data for processing an employee promotion.
+ *
+ * <p>This DTO is used to package the target employee ID and all new attributes (salary, department, and title)
+ * in a single object for transmission between the client and the server, typically as part of a RESTful request body.</p>
+ *
+ * * This DTO is used to facilitate the business operation of promoting an employee.
+ * * It is **not persisted** to the database and does not directly map to a single entity.
+ * * @author Ilyas & Wei Xian
+ *
  */
 public class PromotionDTO  {
+
+    /**
+     * The unique employee number of the employee being promoted.
+     */
     public int empNo;
+
+    /**
+     * The **new** salary amount associated with the promotion.
+     */
     public int salary;
+
+    /**
+     * The ID of the **new** department the employee is moving to, if applicable.
+     */
     public String departmentID;
+
+    /**
+     * The **new** job title the employee will receive.
+     */
     public String title;
 
+    /**
+     * Default public constructor.
+     */
     public PromotionDTO() {}
 
+    /**
+     * Parameterized constructor for creating a {@code PromotionDTO} instance.
+     *
+     * @param employeeID The employee number.
+     * @param salary The new salary amount.
+     * @param departmentID The new department ID.
+     * @param title The new title.
+     */
     public PromotionDTO(int employeeID, int salary, String departmentID, String title) {
         this.empNo = employeeID;
         this.salary = salary;
@@ -22,42 +54,77 @@ public class PromotionDTO  {
         this.title = title;
     }
 
+    /**
+     * Retrieves the new title.
+     * @return The job title string.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the new title.
+     * @param title The title string to set.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Retrieves the new department ID.
+     * @return The department ID string.
+     */
     public String getDepartmentID() {
         return departmentID;
     }
 
+    /**
+     * Sets the new department ID.
+     * @param departmentID The department ID string to set.
+     */
     public void setDepartmentID(String departmentID) {
         this.departmentID = departmentID;
     }
 
+    /**
+     * Retrieves the new salary amount.
+     * @return The salary amount.
+     */
     public int getSalary() {
         return salary;
     }
 
+    /**
+     * Sets the new salary amount.
+     * @param salary The salary amount to set.
+     */
     public void setSalary(int salary) {
         this.salary = salary;
     }
 
+    /**
+     * Retrieves the employee number.
+     * @return The employee number.
+     */
     public int getEmpNo() {
         return empNo;
     }
 
+    /**
+     * Sets the employee number.
+     * @param empNo The employee number to set.
+     */
     public void setEmpNo(int empNo) {
         this.empNo = empNo;
     }
 
+    /**
+     * Returns a string representation of the {@code PromotionDTO} object.
+     *
+     * @return A formatted string showing the promotion details.
+     */
     @Override
     public String toString() {
         return "promotion: " +empNo + "\t" + salary + "\t" + departmentID + "\t" + title;
     }
-
-
 }
